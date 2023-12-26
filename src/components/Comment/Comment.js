4f59722604e8c5ee677c506add31312109edb259
Anton Chupreta <anton.chupreta@gmail.com>
2023-12-26 22:59:51 +0300

    link problem fixed

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -6,13 +6,35 @@ import './Comment.css'
 
 const Comment = (props) => {
     const { comment } = props;
+    
+    const linkPattern = /\[([^\]]+)\]\(([^)]+)\)/g;
+    
+    const modifiedComment = {...comment}
+
+    try {
+    modifiedComment.body = modifiedComment.body.replace(linkPattern, (match, linkName, url) => {
+        
+        if (linkName.includes('http')) {
+          return `[Link](${url})`;
+        } else {
+          
+          return match;
+        }
+      });
+    } catch(error) {
+        console.log(error)
+    }
+
+    
     return (
         <div className="comment">
             <div className="comment-metadata">
                 <p className="comment-author">{comment.author}</p>
                 <p className="comment-created-time">{moment.unix(comment.created_utc).fromNow()}</p>
             </div>
-            <ReactMarkdown children={comment.body} />
+            <div className="comment-body">
+                <ReactMarkdown children={modifiedComment.body} />
+            </div>
         </div>
     )
 }
